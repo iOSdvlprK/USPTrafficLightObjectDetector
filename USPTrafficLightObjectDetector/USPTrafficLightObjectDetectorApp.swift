1fa05e75c966d8df25caef8496f0abde072b3c77
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-27 21:01:39 +0900

    Initial Commit

---
+++ USPTrafficLightObjectDetector/USPTrafficLightObjectDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPTrafficLightObjectDetectorApp.swift
+//  USPTrafficLightObjectDetector
+//
+//  Created by joe on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct USPTrafficLightObjectDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
